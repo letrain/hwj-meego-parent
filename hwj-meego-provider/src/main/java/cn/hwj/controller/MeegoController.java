f75d6a4fb8109d6d8ddae88ba94c73d5ff7d4af4
huangwenjie <letrain47@outlook.com>
2020-06-26 11:18:56 +0800

    [modify]添加日志输出

--- hwj-meego-provider/src/main/java/cn/hwj/controller/MeegoController.java
+++ hwj-meego-provider/src/main/java/cn/hwj/controller/MeegoController.java
@@ -2,6 +2,8 @@ package cn.hwj.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/meego")
 public class MeegoController {
 
+    private Logger logger = LoggerFactory.getLogger(MeegoController.class);
+
     @PostMapping("/N9")
     public String meegoN9Like(@RequestBody JSONObject params){
+        logger.info("请求参数：{}", JSONObject.toJSONString(params));
         JSONObject result = new JSONObject();
         result.put("model", "Meego-N9");
+        logger.info("返回结果：{}", JSONObject.toJSONString(result));
         return JSON.toJSONString(result);
     }
 }
